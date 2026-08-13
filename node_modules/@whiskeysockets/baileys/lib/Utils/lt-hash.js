24882293c6a90e9e0c886b88830c0a0b101fa807
werkito <zapoteco1234567@gmail.com>
2026-08-12 21:21:24 -0600

    Delete node_modules directory

--- node_modules/@whiskeysockets/baileys/lib/Utils/lt-hash.js
+++
@@ -1,8 +0,0 @@
-import { LTHashAntiTampering } from 'whatsapp-rust-bridge';
-/**
- * LT Hash is a summation based hash algorithm that maintains the integrity of a piece of data
- * over a series of mutations. You can add/remove mutations and it'll return a hash equal to
- * if the same series of mutations was made sequentially.
- */
-export const LT_HASH_ANTI_TAMPERING = new LTHashAntiTampering();
-//# sourceMappingURL=lt-hash.js.map
