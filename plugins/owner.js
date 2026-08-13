580a3828e286548397f127e49bec99dfd914e95b
werkito <zapoteco1234567@gmail.com>
2026-08-12 22:19:25 -0600

    Refactor owner list and update handler command

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,19 +1,18 @@
-let handler = async (m, { conn }) => {
-let owners = [
-  '527444200627',
-  '52XXXXXXXXXX', // <- aquí agregas el segundo
-  '52XXXXXXXXXX'  // <- aquí agregas el tercero
+// AQUÍ PONES TODOS LOS OWNERS CON PODERES
+global.owner = [
+  ['527444200627', 'zapoteco1212 👑', true],
+  ['52XXXXXXXXXX', 'Owner 2', true], // segundo
+  ['52XXXXXXXXXX', 'Owner 3', true]  // tercero
 ]
 
+let handler = async (m, { conn }) => {
 let texto = `💮 *OWNERS - WADI-BOT v3.0*\n\n`
-for (let num of owners) {
-texto += `👑 wa.me/${num}\n`
+for (let [num, nombre] of global.owner) {
+texto += `👑 ${nombre}\n📱 wa.me/${num}\n\n`
 }
-texto += `\nSi necesitas ayuda escribe.`
-
 await conn.sendMessage(m.chat, { text: texto }, { quoted: m })
 }
 handler.help = ['owner']
 handler.tags = ['info']
-handler.command = ['owner', 'creador']
+handler.command = ['owner', 'creador', 'dueño']
 module.exports = handler
