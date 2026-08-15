a4049d0413fa3919d29111e4f59086ff236e9958
werkito <zapoteco1234567@gmail.com>
2026-08-14 22:27:21 -0600

    Add global configuration for WADI-BOT v3.0

---
+++ config.js
@@ -0,0 +1,7 @@
+global.botname = 'WADI-BOT v3.0'
+global.packname = '💮 WADI-BOT v3.0 🌊'
+global.author = 'by zapoteco1212 👑'
+global.wm = '💮 WADI-BOT v3.0 - by zapoteco1212'
+global.owner = [
+  ['527444200627', 'zapoteco1212 👑', true]
+]
